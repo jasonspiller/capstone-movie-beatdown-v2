f0f1de2fee47d016142437fd3a37cbc25df51991
Jason Spiller <jason@thetargetlab.com>
2018-06-04 15:16:48 -0600

    Google callback function working.

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -8,7 +8,8 @@ passport.use(
     callbackURL: '/auth/google/redirect',
     clientID: keys.google.clientID,
     clientSecret: keys.google.clientSecret
-  }, () => {
+  }, (accessToken, refreshToken, profile, done) => {
     // passport callback
+    console.log(profile);
   })
 )
