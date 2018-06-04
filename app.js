7d45cb372b3c618d5b6b38ae271b1354e1eb7978
Jason Spiller <jason@thetargetlab.com>
2018-06-04 15:26:11 -0600

    Added MongoDB.

--- app.js
+++ app.js
@@ -2,11 +2,18 @@ const express = require('express');
 const path = require('path');
 const authRoutes = require('./routes/auth-routes');
 const passportSetup = require('./config/passport-setup');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
 
 const app = express();
 
 // setup view engine
 app.set('view engine', 'ejs');
+
+// connect to mongodb
+mongoose.connect(keys.mongodb.dbURI, () => {
+  console.log('Connected to DB.');
+})
 
 // set public files directory
 app.use(express.static(path.join(__dirname, 'public')));
