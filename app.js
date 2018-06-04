07664fa24e7cf4576867d74cce9103ff799a45ed
Jason Spiller <jason@thetargetlab.com>
2018-06-04 16:32:48 -0600

    Added session cookie.

--- app.js
+++ app.js
@@ -4,11 +4,25 @@ const authRoutes = require('./routes/auth-routes');
 const passportSetup = require('./config/passport-setup');
 const mongoose = require('mongoose');
 const keys = require('./config/keys');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 
 const app = express();
 
 // setup view engine
 app.set('view engine', 'ejs');
+
+
+app.use(cookieSession({
+  // set age to one day
+  maxAge: 24 * 60 * 60 * 1000,
+  // encrypt cookie
+  keys: [keys.session.cookieKey]
+}));
+
+// initialize passport
+app.use(passport.initialize());
+app.use(passport.session());
 
 // connect to mongodb
 mongoose.connect(keys.mongodb.dbURI, () => {
