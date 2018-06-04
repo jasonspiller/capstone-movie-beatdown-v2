b96bedba062e43024b3b3eb57b6d4b47ee88133d
Jason Spiller <jason@thetargetlab.com>
2018-06-04 15:46:52 -0600

    Added ability to save User.

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,15 +1,23 @@
 const passport = require('passport');
 const googleStrategy = require('passport-google-oauth20')
 const keys = require('./keys');
+const User = require('../models/user');
 
 passport.use(
   new googleStrategy({
+
     // options for the google strategy
     callbackURL: '/auth/google/redirect',
     clientID: keys.google.clientID,
     clientSecret: keys.google.clientSecret
   }, (accessToken, refreshToken, profile, done) => {
-    // passport callback
-    console.log(profile);
+
+    // passport callback creates new user
+    new User({
+      username: profile.displayName,
+      googleId: profile.id
+    }).save().then((newUser) => {
+      console.log('New user created: ' + newUser);
+    })
   })
 )
